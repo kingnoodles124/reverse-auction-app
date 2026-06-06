31e78b0b51a2fb633fffeb617bdc0f1218992e57
kingnoodles124 <rawaau124@gmail.com>
2026-06-06 18:00:16 +0500

    Add cloud database config

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ const dialogflow =
 const uuid =
     require('uuid');
 
+console.log("HOST:", process.env.DB_HOST);
+console.log("USER:", process.env.DB_USER);
+console.log("DB:", process.env.DB_NAME);
+console.log("PORT:", process.env.DB_PORT);
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: 'sql.freedb.tech',
+    user: 'u_ymuwps',
+    password: 'O8yIqa5UZZLV',
+    database: 'freedb_R9BTAh9R',
     port: process.env.DB_PORT
 });
 
